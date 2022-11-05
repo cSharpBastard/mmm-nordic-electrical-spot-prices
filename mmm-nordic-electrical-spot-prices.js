668ab40ec18c24e1ae881f69c5d6e736f115d308
Jonas Katona <jonas.katona@preciofishbone.se>
2022-11-05 13:44:02 +0100

    Add project files.

---
+++ mmm-nordic-electrical-spot-prices.js
@@ -0,0 +1,81 @@
+'use strict';
+
+Module.register("mmm-nordic-electrical-spot-prices", {
+    requiresVersion: "2.1.0",
+    defaults: {
+        currency: "SEK", // can also be 'DKK', 'EUR', 'NOK'
+        area: "SE3", // See http://www.nordpoolspot.com/maps/
+        updateInterval: 60000,
+        title: "Dagens priser",
+        yLegend: "Kr / kWh"
+    },
+    dataPoints: {},
+    chart: {},
+    getScripts: function () {
+        return [
+            this.file('moment.min.js'),
+            this.file('canvasjs.min.js')
+        ];
+    },
+    start: function () {
+        const self = this;
+        this.shareConfig();
+        setTimeout(function () { self.getData() }, 3000);
+        this.scheduleUpdate();
+    },
+    scheduleUpdate: function (delay) {
+        const self = this;
+        console.log("mmm-nordic-electrical-spot-prices scheduleUpdate Start")
+        var nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+            nextLoad = delay;
+        }
+        
+        setInterval(function () { self.getData() }, nextLoad);
+    },
+    getHeader: function () {
+        return this.config.title;
+    },
+    shareConfig: function () {
+        this.sendSocketNotification('SET_CONFIG', this.config);
+	},
+    getData: function () {
+        const date = moment(new Date());
+        this.sendSocketNotification('GET_SPOTDATA', date);
+	},
+    getDom: function () {
+        var self = this;
+        var wrapper = document.createElement("div");
+        wrapper.id = "chartContainer";
+        setTimeout(function () { self.createChart(wrapper.id); }, 1000);
+        return wrapper;
+    },
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "SPOT_RECEIVED") {
+            this.dataPoints = payload;
+            this.updateDom(this.config.fadeSpeed);
+        }
+    },
+    createChart: function (id) {
+        this.chart = new CanvasJS.Chart(id, {
+            animationEnabled: false,
+            theme: "dark1",
+            axisY: {
+                title: this.config.yLegend,
+                titleFontSize: 24
+            },
+            axisX: {
+                interval: 1,
+                indexLabelColor: "red"
+			},
+            data: [{
+                type: "column",
+                yValueFormatString: "#.00",
+                showInLegend: false, 
+                color: "gray",
+                dataPoints: this.dataPoints
+            }]
+        });
+        this.chart.render();
+	}
+});
---
+++ node_helper.js
@@ -0,0 +1,68 @@
+'use strict';
+const NodeHelper = require('node_helper');
+const request = require('request');
+const moment = require('moment');
+
+module.exports = NodeHelper.create({
+    config: {},
+    start: function () { },
+    getData: function (p_date) {
+        const self = this;
+        const date = moment(p_date);
+
+        var url = 'https://www.nordpoolgroup.com/api/marketdata/page/10?currency=';
+        url = url + this.config.currency + ',' + this.config.currency + ',' + this.config.currency;
+        url = url + '&endDate=' + date.format('dd-MM-yyyy');
+        request({ url: url, method: 'GET' }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var result = JSON.parse(body);
+                self.gotData(result);
+            }
+        });
+    },
+    gotData: function (result) {
+        try {
+            const now = moment(new Date());
+            const data = result.data;
+            if (data && data.Rows && data.Rows.length) {
+                const values = []
+                for (const row of data.Rows) {
+                    if (row.IsExtraRow) {
+                        continue
+                    }
+                    const date = moment(row.StartTime, 'YYYY-MM-DDTHH:mm:ss', true);
+                    if (!date.isValid()) {
+                        continue
+                    }
+
+                    for (const column of row.Columns) {
+                        const value = parseFloat(column.Value.replace(/,/, '.').replace(/ /g, '')) / 1000
+                        if (isNaN(value)) {
+                            continue
+                        }
+                        const area = column.Name
+                        if (this.config.area.indexOf(area) >= 0) {
+                            if (date.format('HH') === now.format('HH'))
+                                values.push({ label: date.format('HH'), y: value, color: "red", indexLabel: value });
+                            else
+                                values.push({ label: date.format('HH'), y: value })
+                        }
+                    }
+                }
+                this.sendSocketNotification('SPOT_RECEIVED', values);
+            }
+            else
+                console.log("Invalid data:" + data);
+        } catch (ex) {
+            console.error(ex);
+		}
+	},
+    //Subclass socketNotificationReceived received.
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'GET_SPOTDATA') {
+            this.getData(payload);
+        } else if (notification === 'SET_CONFIG') {
+            this.config = payload;
+		}
+    }
+});
