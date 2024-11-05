2d520e68f1e1b15d9c5bbdaac28f96ed1d039413
Jonas Katona <74971134+cSharpBastard@users.noreply.github.com>
2024-11-05 16:31:17 +0100

    Change to different source

--- node_helper.js
+++ node_helper.js
@@ -9,10 +9,7 @@ module.exports = NodeHelper.create({
     getData: function (p_date) {
         const self = this;
         const date = moment(p_date);
-	// CHange to new datasource: https://www.elprisetjustnu.se/api/v1/prices/2024/11-01_SE3.json
-        var url = 'https://www.nordpoolgroup.com/api/marketdata/page/10?currency=';
-        url = url + this.config.currency + ',' + this.config.currency + ',' + this.config.currency;
-        url = url + '&endDate=' + date.format('dd-MM-yyyy');
+        var url = 'https://www.elprisetjustnu.se/api/v1/prices/2024/11-01_SE3.json';
         request({ url: url, method: 'GET' }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var result = JSON.parse(body);
@@ -23,41 +20,33 @@ module.exports = NodeHelper.create({
     gotData: function (result) {
         try {
             const now = moment(new Date());
-            const data = result.data;
-            if (data && data.Rows && data.Rows.length) {
+            const data = result;
+            if (data) {
                 const values = []
-                for (const row of data.Rows) {
-                    if (row.IsExtraRow) {
-                        continue
-                    }
-                    const date = moment(row.StartTime, 'YYYY-MM-DDTHH:mm:ss', true);
+                for (const row of data) {
+                    const date = moment(row.time_start, 'YYYY-MM-DDTHH:mm:ss', true);
                     if (!date.isValid()) {
                         continue
                     }
 
-                    for (const column of row.Columns) {
-                        const value = parseFloat(column.Value.replace(/,/, '.').replace(/ /g, '')) / 1000
-                        if (isNaN(value)) {
-                            continue
-                        }
-                        const area = column.Name
-                        if (this.config.area.indexOf(area) >= 0) {
-                            if (date.format('HH') === now.format('HH'))
-                                values.push({ label: date.format('HH'), y: value, color: "blue", indexLabel: value });
-                            else
-                            {
-				if(value < 1.0)
-                                	values.push({ label: date.format('HH'), y: value, color: "green" })
-                                else if(value < 2.0)
-                                	values.push({ label: date.format('HH'), y: value, color: "yellow" })
-                                else if(value < 3.0)
-                                	values.push({ label: date.format('HH'), y: value, color: "gold" })
-                                else if(value < 4.0)
-                                	values.push({ label: date.format('HH'), y: value, color: "orange" })
-				else
-                                	values.push({ label: date.format('HH'), y: value, color: "red" })
-                            }
-                        }
+                    const value = parseFloat(row.SEK_per_kWh)
+                    if (isNaN(value)) {
+                        continue
+                    }
+                    if (date.format('HH') === now.format('HH'))
+                        values.push({ label: date.format('HH'), y: value, color: "blue", indexLabel: value });
+                    else
+                    {
+                       if(value < 1.0)
+                            values.push({ label: date.format('HH'), y: value, color: "green" })
+                        else if(value < 2.0)
+                            values.push({ label: date.format('HH'), y: value, color: "yellow" })
+                        else if(value < 3.0)
+                            values.push({ label: date.format('HH'), y: value, color: "gold" })
+                        else if(value < 4.0)
+                            values.push({ label: date.format('HH'), y: value, color: "orange" })
+                        else
+                            values.push({ label: date.format('HH'), y: value, color: "red" })
                     }
                 }
                 this.sendSocketNotification('SPOT_RECEIVED', values);
